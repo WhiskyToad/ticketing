fbeeedf875d7757fa58d1ea59734f23cf5c96dc6
Steven Craig <stevencraig321@gmail.com>
2023-10-13 07:34:57 +0100

    feat: linters

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,26 +1,24 @@
-import express, {Request, Response} from 'express'
-import { body, validationResult } from 'express-validator'
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/api/users/signup',
-[
-    body('email')
-    .isEmail()
-    .withMessage('Email must be valid'),
-    body('password')
-    .trim()
-    .isLength({min: 4, max: 30})
-    .withMessage('Password must be between 4 and 20 characters')
-], (req: Request, res: Response) => {
-    const errors = validationResult(req)
-    if(!errors.isEmpty()) {
-        return res.status(400).send(errors.array())
+router.post(
+  '/api/users/signup',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password').trim().isLength({ min: 4, max: 30 }).withMessage('Password must be between 4 and 20 characters'),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array());
     }
-    const {email, password} = req.body
+    const { email, password } = req.body;
 
-    console.log('creating user')
-    res.send({})
-})
+    console.log('creating user');
+    res.send({});
+  },
+);
 
-export {router as signupRouter}
+export { router as signupRouter };
