2aedee88c18f76fe7b65548df1057a6508ac415a
Steven Craig <stevencraig321@gmail.com>
2023-10-25 08:16:59 +0100

    feat: validate request

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
-import { BadRequstError, RequestValidationError } from '../errors';
+import { BadRequstError } from '../errors';
 import { User } from '../models/user';
 import jwt from 'jsonwebtoken';
 
@@ -12,12 +12,8 @@ router.post(
     body('email').isEmail().withMessage('Email must be valid'),
     body('password').trim().isLength({ min: 4, max: 30 }).withMessage('Password must be between 4 and 20 characters'),
   ],
+  validationResult,
   async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
     const { email, password } = req.body;
 
     const existingUser = await User.findOne({ email });
